81df88565228b6787077205111057b51735318c9
David Puerto <dsp5502@gmail.com>
2021-10-06 11:00:43 -0500

    agregar explciacion saas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/styles.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Fondo1">este es un ejemplo 1</div>
+      <div className="Fondo2">este es un ejemplo 2</div>
+      <div className="Fondo3">este es un ejemplo 3</div>
+     
     </div>
   );
 }
